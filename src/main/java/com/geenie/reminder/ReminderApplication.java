3d4f48d9870cecb177cc2da0db80c0b26d1a612d
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-04-15 01:22:41 +0100

    configure spring boot with javafx

--- src/main/java/com/geenie/reminder/ReminderApplication.java
+++ src/main/java/com/geenie/reminder/ReminderApplication.java
@@ -1,12 +1,55 @@
-package com.geenie;
+package com.geenie.reminder;
 
-import org.springframework.boot.SpringApplication;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.geenie.reminder.config.StageManager;
+import com.geenie.reminder.view.FxmlView;
+
 
 @SpringBootApplication
-public class ReminderApplication {
+public class ReminderApplication extends Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReminderApplication.class, args);
-	}
+    protected ConfigurableApplicationContext springContext;
+    protected StageManager stageManager;
+
+    public static void main(final String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        springContext = springBootApplicationContext();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stageManager = springContext.getBean(StageManager.class, stage);
+        displayInitialScene();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        springContext.close();
+    }
+
+    /**
+     * Useful to override this method by sub-classes wishing to change the first
+     * Scene to be displayed on startup. Example: Functional tests on main
+     * window.
+     */
+    protected void displayInitialScene() {
+        stageManager.switchScene(FxmlView.LOGIN);
+    }
+
+    
+    private ConfigurableApplicationContext springBootApplicationContext() {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(ReminderApplication.class);
+        String[] args = getParameters().getRaw().stream().toArray(String[]::new);
+        return builder.run(args);
+    }
+
 }
