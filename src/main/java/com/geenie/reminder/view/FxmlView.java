fab4821aab29af72f52d5f4e417f4b55b74dc1eb
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-04-16 21:39:18 +0100

    add main menu in view

--- src/main/java/com/geenie/reminder/view/FxmlView.java
+++ src/main/java/com/geenie/reminder/view/FxmlView.java
@@ -1,28 +1,38 @@
 package com.geenie.reminder.view;
 
-
 import java.util.ResourceBundle;
 
 public enum FxmlView {
 
-    LOGIN {
-        @Override
+	LOGIN {
+		@Override
 		public String getTitle() {
-            return getStringFromResourceBundle("login.title");
-        }
+			return getStringFromResourceBundle("login.title");
+		}
 
-        @Override
+		@Override
 		public String getFxmlFile() {
-            return "/fxml/login.fxml";
-        }
-    };
-    
-    public abstract String getTitle();
-    public abstract String getFxmlFile();
-    
-    String getStringFromResourceBundle(String key){
-        return ResourceBundle.getBundle("Bundle").getString(key);
-    }
+			return "/fxml/login.fxml";
+		}
+	},
+	MAIN_MENU {
+		@Override
+		public String getTitle() {
+			return getStringFromResourceBundle("main_menu.title");
+		}
+
+		@Override
+		public String getFxmlFile() {
+			return "/fxml/mainMenu.fxml";
+		}
+	};
+
+	public abstract String getTitle();
+
+	public abstract String getFxmlFile();
+
+	String getStringFromResourceBundle(String key) {
+		return ResourceBundle.getBundle("Bundle").getString(key);
+	}
 
 }
-
