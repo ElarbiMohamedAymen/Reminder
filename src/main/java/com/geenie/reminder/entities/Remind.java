57283ad4913f23770b3008f1bb5b19b5460a0481
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-04-16 21:36:18 +0100

    add remind entity

---
+++ src/main/java/com/geenie/reminder/entities/Remind.java
@@ -0,0 +1,98 @@
+package com.geenie.reminder.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.geenie.reminder.enumerations.State;
+
+@Entity
+public class Remind implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idReminder;
+	private State state;
+	private String description;
+	private String date;
+	private String time;
+	
+	public Remind() {
+		super();
+	}
+	public long getIdReminder() {
+		return idReminder;
+	}
+	public void setIdReminder(long idReminder) {
+		this.idReminder = idReminder;
+	}
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	@Override
+	public String toString() {
+		return "Remind [idReminder=" + idReminder + ", state=" + state + ", description=" + description + ", date="
+				+ date + ", time=" + time + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((time == null) ? 0 : time.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Remind other = (Remind) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (time == null) {
+			if (other.time != null)
+				return false;
+		} else if (!time.equals(other.time))
+			return false;
+		return true;
+	}
+	
+	
+
+}
