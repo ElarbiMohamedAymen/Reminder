b8ae6a71445389a92f3b6aecb175a301edbcedd5
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-04-18 22:37:33 +0100

    Add abstract method to display custom name

--- src/main/java/com/geenie/reminder/enumerations/Priority.java
+++ src/main/java/com/geenie/reminder/enumerations/Priority.java
@@ -1,7 +1,28 @@
 package com.geenie.reminder.enumerations;
 
-public enum State {
-	URGENT,
-	PAS_VRAIMENT,
-	NORMAL
+public enum Priority {
+	URGENT {
+
+		@Override
+		public String getStatus() {
+			return "Urgent";
+		}
+	},
+	PAS_VRAIMENT {
+
+		@Override
+		public String getStatus() {
+			return "Pas Vraiment";
+		}
+	},
+	NORMAL {
+
+		@Override
+		public String getStatus() {
+			return "Normal";
+		}
+	};
+
+	public abstract String getStatus();
+	
 }
