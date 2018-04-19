a190456bcf2e46e89082e8c3f84d25f587c59dc8
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-04-19 18:58:57 +0100

    Adding values for dev-database

---
+++ src/main/java/com/geenie/reminder/StartUpInit.java
@@ -0,0 +1,46 @@
+package com.geenie.reminder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.geenie.reminder.entities.Remind;
+import com.geenie.reminder.enumerations.Priority;
+import com.geenie.reminder.service.interfaces.IRemindMySQLService;
+
+
+
+@Service
+@Transactional
+public class StartUpInit implements ApplicationRunner {
+
+	@Autowired
+	@Qualifier("remindMySQLServiceImpl")
+	IRemindMySQLService remindService;
+	
+	@Override
+	public void run(ApplicationArguments arg0) throws Exception {
+		Remind remind = new Remind();
+		Remind remind2 = new Remind();
+		Remind remind3 = new Remind();
+		
+		/*Remind */
+		remind.setName("Preparer le dinner");
+		remind.setDescription("La description pour la préparation du dinner");
+		remind.setPriority(Priority.NORMAL);
+		/*Remind2 */
+		remind2.setName("Faire sortir la poubelle");
+		remind2.setDescription("Description pour la poubelle");
+		remind2.setPriority(Priority.URGENT);
+		/*Remind3 */
+		remind3.setName("Faire les courses");
+		remind3.setDescription("Acheter les legumes");
+		remind3.setPriority(Priority.PAS_VRAIMENT);
+		remindService.addEvent(remind);
+		remindService.addEvent(remind2);
+		remindService.addEvent(remind3);
+	}
+}
