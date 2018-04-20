8896b33c5651fbe70e790b289184e5a298bee352
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-04-20 23:08:43 +0100

    add state

--- src/main/java/com/geenie/reminder/StartUpInit.java
+++ src/main/java/com/geenie/reminder/StartUpInit.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.geenie.reminder.entities.Remind;
 import com.geenie.reminder.enumerations.Priority;
+import com.geenie.reminder.enumerations.State;
 import com.geenie.reminder.service.interfaces.IRemindMySQLService;
 
 
@@ -31,14 +32,17 @@ public class StartUpInit implements ApplicationRunner {
 		remind.setName("Preparer le dinner");
 		remind.setDescription("La description pour la préparation du dinner");
 		remind.setPriority(Priority.NORMAL);
+		remind.setState(State.TODO);
 		/*Remind2 */
 		remind2.setName("Faire sortir la poubelle");
 		remind2.setDescription("Description pour la poubelle");
 		remind2.setPriority(Priority.URGENT);
+		remind2.setState(State.DOING);
 		/*Remind3 */
 		remind3.setName("Faire les courses");
 		remind3.setDescription("Acheter les legumes");
 		remind3.setPriority(Priority.PAS_VRAIMENT);
+		remind3.setState(State.DONE);
 		remindService.addEvent(remind);
 		remindService.addEvent(remind2);
 		remindService.addEvent(remind3);
